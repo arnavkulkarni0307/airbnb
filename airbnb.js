f9707b28f63eba39af26bc8d28c035b02cbdf263
arnavkulkarni0307 <68114100+arnavkulkarni0307@users.noreply.github.com>
2020-08-30 18:20:29 +0530

    Add files via upload

---
+++ airbnb.js
@@ -0,0 +1,12 @@
+function search(){
+
+var where = document.getElementById('Where').value;
+var checkin = document.getElementById('checkin').value;
+var checkout = document.getElementById('checkout').value;
+var guest = document.getElementById('Guests').value;
+
+// var result = " Your Location :-"+where + ", Checkin Date:- "
+// + checkin +", Checkout Date:- " + checkout + ", Number of Guests:- "+ guest;
+
+alert('Your response was submitted,THANK YOU ..');
+}
